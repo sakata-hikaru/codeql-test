a86bdce9763b5cc3cf116862e30c40ac91538e0d
hikaru.sakata <hikaru.sakata@rakus.co.jp>
2024-06-25 10:27:57 +0900

    initial

---
+++ src/test/java/org/example/codeqltest/CodeqlTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.codeqltest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeqlTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
